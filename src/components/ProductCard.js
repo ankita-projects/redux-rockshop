d13a50bc4733a6b085d9df40b9f5bd803f8ff90c
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-09-29 18:17:08 +0200

    adding card styling

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,27 +7,37 @@ const ProductCard = (input) => {
   let product = input.product;
   const dispatch = useDispatch();
   return (
-    <li>
-      <div className="card">
-        <img src={`${product.image}`} alt={product.title} />
-        <div className="card-title">
-          <h3>{product.title}</h3>
+
+      <section className="card-container">
+        <div className="card">
+           <div className="my-1/2 px-1/2 w-auto md:w-9/12 lg:my-4 lg:px-4 lg:w-102">
+              <article className="overflow-hidden rounded-lg shadow-lg">
+          <img src={`${product.image}`} alt={product.title} />
+          <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+            <h3>{product.title}</h3>
+         
+            <p className=" text-opacity-100 text-black">
+            {product.description}
+            </p>
+          <h2>€{product.price}</h2>
+          <button onClick={() => dispatch(addProduct(product))}>
+            Add to cart
+          </button>
+          <div
+            className="rock__buttoncontainer__cart"
+            onClick={() => {
+              console.log("button clicked");
+            }}
+          >
+            <AddShoppingCart style={{ fontSize: 30 }} />
+          </div>
+      </header>
+        </article>
+      
         </div>
-        <div className="card-content">
-          <p>{product.description}</p>
         </div>
-        <h2>€{product.price}</h2>
-        <button onClick={() => dispatch(addProduct(product))}>Add to cart</button>
-        <div
-          className="rock__buttoncontainer__cart"
-          onClick={() => {
-            console.log("button clicked");
-          }}
-        >
-          <AddShoppingCart style={{ fontSize: 30 }} />
-        </div>
-      </div>
-    </li>
+      </section>
+   
   );
 };
 
